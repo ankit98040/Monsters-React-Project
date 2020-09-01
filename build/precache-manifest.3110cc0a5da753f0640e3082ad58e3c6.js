ee289a160e9cfd3a94aa2c6ca81fc28ed3bc52c9
Pramanik, Ankit <pramanik.85849@gmail.com>
2020-09-01 18:23:29 +0530

    updates

---
+++ build/precache-manifest.3110cc0a5da753f0640e3082ad58e3c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2fd61d89395988bfbb40e4bb1d6f1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e359b7d0414ff236d1e6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2a8d18c60ae01bb7221",
+    "url": "/static/js/2.ebf292fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ebf292fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e359b7d0414ff236d1e6",
+    "url": "/static/js/main.3b5d9a04.chunk.js"
+  },
+  {
+    "revision": "6ee33a9eb5113d8c4c15",
+    "url": "/static/js/runtime-main.52709719.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
